11e07f383f7c0216e41bf358839aaeac81e98ef6
Fitri Nur Azis <fitnas30@gmail.com>
2025-05-08 20:26:25 +0700

    feat: menambahkan fitur tambah transaksi

--- app.sh
+++ app.sh
@@ -7,6 +7,40 @@ YELLOW="\e[33m"
 CYAN="\e[36m"
 NC="\e[0m" #No Color
 
+
+# Array penyimpanan data
+declare -a jenis_transaksi
+declare -a nominal_transaksi
+declare -a deskripsi_transaksi
+
+# Fungsi: Tambah Transaksi
+tambah_transaksi() {
+    echo -e "${CYAN}=== Tambah Transaksi ===${RESET}"
+    read -p "Jenis (masuk/keluar): " jenis
+    if [[ "$jenis" != "masuk" && "$jenis" != "keluar" ]]; then
+        echo -e "${RED}Jenis transaksi tidak valid!${RESET}"
+        return
+    fi
+
+    read -p "Nominal: " nominal
+    if ! [[ "$nominal" =~ ^[0-9]+$ ]]; then
+        echo -e "${RED}Nominal harus berupa angka!${RESET}"
+        return
+    fi
+
+    read -p "Deskripsi: " deskripsi
+    if [[ -z "$deskripsi" ]]; then
+        echo -e "${RED}Deskripsi tidak boleh kosong!${RESET}"
+        return
+    fi
+
+    jenis_transaksi+=("$jenis")
+    nominal_transaksi+=("$nominal")
+    deskripsi_transaksi+=("$deskripsi")
+    simpan_data
+    echo -e "${GREEN}Transaksi berhasil ditambahkan!${RESET}"
+}
+
 # Menu Utama
 while true; do
     echo -e "\n${CYAN}===== Aplikasi Catatan Keuangan =====${NC}"
@@ -16,4 +50,8 @@ while true; do
     echo "4. Hapus Transaksi"
     echo "5. Keluar"
     read -p "Pilih opsi [1-5]: " pilihan
+
+    case $pilihan in
+        1) tambah_transaksi ;;
+    esac
 done
